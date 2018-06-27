fd6acc0da3ce0c590846421d459ac06e7a67b283
Jaaaaaaaaaannnn <janp.t2000@gmail.com>
2018-06-27 08:32:01 +0200

    update java gui

--- src/main/java/ui/LoadSchedule.java
+++ src/main/java/ui/LoadSchedule.java
@@ -1,31 +1,20 @@
 package ui;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 
-import java.io.File;
-import java.net.URL;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
-import java.nio.file.Files;
+import java.util.Objects;
 
 public class LoadSchedule extends Application {
-    @FXML
-    private ListView schedulesListView;
+    //@FXML
+    //private ListView schedulesListView;
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("LoadSchedule.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource("LoadSchedule.fxml")));
 
         Scene scene = new Scene(root, 500, 250);
 
@@ -37,6 +26,7 @@ public class LoadSchedule extends Application {
 
     @FXML
     public void initialize(){
+        /*
         File schedulesDirectory = new File(System.getProperty("user.dir").toString() + "\\schedules\\");
         if(schedulesDirectory.isDirectory()){
             File[] files = schedulesDirectory.listFiles();
@@ -52,6 +42,6 @@ public class LoadSchedule extends Application {
             // TODO: Create Folder and new Schedule
         }
 
-    }
+    */}
 
 }
